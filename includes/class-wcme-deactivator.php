804895519e0ca5b44baab0dff0875f8e1c0282ea
Ahmad Wael <dev.ahmedwael@gmail.com>
2020-07-09 15:00:57 +0200

    initial files

---
+++ includes/class-wcme-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/DevWael
+ * @since      1.0.0
+ *
+ * @package    Wcme
+ * @subpackage Wcme/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wcme
+ * @subpackage Wcme/includes
+ * @author     Ahmad Wael <dev.ahmedwael@gmail.com>
+ */
+class Wcme_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
